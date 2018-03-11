1872a154b31b277c5f81dee34adc6a641a625629
Lindsey Trainor <ltrainor38@gmail.com>
2018-03-11 06:01:24 +0000

    automatically backed up by learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -21,7 +21,7 @@ class Scraper
   def self.scrape_profile_page(profile_url)
     doc = Nokogiri::HTML(open(profile_url))
     quote = doc.css('.profile_quote')
-    links = doc.css('a[href]').first.children
+    links = doc.css('@src').first.value
     binding.pry
   end
 
